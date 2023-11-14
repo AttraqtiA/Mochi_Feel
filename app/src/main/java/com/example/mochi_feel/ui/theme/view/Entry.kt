ddfce2424fc5e63e927010c137c0d07608b790c6
AttraqtiA <114904516+AttraqtiA@users.noreply.github.com>
2023-11-14 17:08:33 +0700

    Pembagian

---
+++ app/src/main/java/com/example/mochi_feel/ui/theme/view/Entry.kt
@@ -0,0 +1,15 @@
+package com.example.mochi_feel.ui.theme.view
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun EntryView() {
+
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun EntryPreview() {
+
+}
