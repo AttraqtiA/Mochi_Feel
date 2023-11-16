332e0d13d3ab1f8e48b168ce8252300ac1454e8b
Karyna <karyna.sanjaya@gmail.com>
2023-11-16 17:55:04 +0700

    HelpMain progress

--- app/src/main/java/com/example/mochi_feel/ui/screen/help/HelpMain.kt
+++ app/src/main/java/com/example/mochi_feel/ui/screen/help/HelpMain.kt
@@ -3,9 +3,11 @@ package com.example.mochi_feel.ui.screen.help
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
@@ -20,7 +22,6 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -33,10 +34,15 @@ import com.example.mochi_feel.ui.theme.inter
 @Composable
 fun HelpMainView() {
     Column (
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color(0xFFF9FFFF))
+            .padding(20.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Row (
-
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.Start
         ) {
             Icon(
                 painter = painterResource(id = R.drawable.baseline_keyboard_backspace_24),
@@ -45,10 +51,24 @@ fun HelpMainView() {
                 modifier = Modifier.padding(start = 4.dp)
             )
         }
-        Text (
-            text = "What are you thinking of writing about?"
+        Spacer(modifier = Modifier.height(30.dp))
+        Text(
+            text = "What are you\nthinking of writing\nabout?",
+            style = TextStyle(
+                fontSize = 28.sp,
+                lineHeight = 28.sp,
+                fontFamily = inter,
+                fontWeight = FontWeight(700),
+                color = Color(0xFF238A91),
+                textAlign = TextAlign.Center,
+            ),
+            modifier = Modifier
+                .width(279.dp)
+                .height(93.dp)
         )
-        Box (
+        Spacer(modifier = Modifier.height(30.dp))
+        Column (
+            horizontalAlignment = Alignment.CenterHorizontally,
             modifier = Modifier
                 .shadow(
                     elevation = 30.dp,
@@ -57,34 +77,65 @@ fun HelpMainView() {
                 )
                 .width(148.dp)
                 .height(148.dp)
-                .background(color = Color(0xFF4ABDC0), shape = RoundedCornerShape(size = 10.dp))
+                .background(color = Color(0xFF4ABDC0), shape = RoundedCornerShape(size = 10.dp)),
         ) {
-            Column {
-                Image(
-                    painter = painterResource(id = R.drawable.help_1),
-                    contentDescription = "Something Random Icon",
-                    modifier = Modifier
-                        .width(84.dp)
-                        .height(84.dp)
-                )
-                Text(
-                    text = "Something\nRandom",
-                    style = TextStyle(
-                        fontSize = 20.sp,
-                        lineHeight = 21.sp,
-                        fontFamily = inter,
-                        fontWeight = FontWeight(700),
-                        color = Color(0xFFFFFFFF),
-                        textAlign = TextAlign.Center,
-                    ),
-                    modifier = Modifier
-                        .width(107.dp)
-                        .height(47.dp)
-                )
-            }
+            Image(
+                painter = painterResource(id = R.drawable.help_1),
+                contentDescription = "Something Random Icon",
+                contentScale = ContentScale.FillBounds,
+                modifier = Modifier
+                    .width(88.dp)
+                    .height(88.dp)
+            )
+            Text(
+                text = "Something\nRandom",
+                style = TextStyle(
+                    fontSize = 20.sp,
+                    lineHeight = 21.sp,
+                    fontFamily = inter,
+                    fontWeight = FontWeight(700),
+                    color = Color(0xFFFFFFFF),
+                    textAlign = TextAlign.Center,
+                ),
+                modifier = Modifier
+                    .width(107.dp)
+                    .height(47.dp)
+            )
+        }
+        Spacer(modifier = Modifier.height(50.dp))
+        Column (
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .width(148.dp)
+                .height(148.dp)
+                .background(color = Color(0xFF4ABDC0), shape = RoundedCornerShape(size = 10.dp)),
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.help_2),
+                contentDescription = "My Emotions Icon",
+                contentScale = ContentScale.FillBounds,
+                modifier = Modifier
+                    .width(88.dp)
+                    .height(88.dp)
+            )
+            Text(
+                text = "My\nEmotions",
+                style = TextStyle(
+                    fontSize = 20.sp,
+                    lineHeight = 21.sp,
+                    fontFamily = inter,
+                    fontWeight = FontWeight(700),
+                    color = Color(0xFFFFFFFF),
+                    textAlign = TextAlign.Center,
+                ),
+                modifier = Modifier
+                    .width(107.dp)
+                    .height(47.dp)
+            )
         }
     }
 }
+
 
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
